6687e35b29c07c05705c8a97b660d6ec35cf013e
malteki <mki.dev@web.de>
2024-10-17 19:02:33 +0200

    added timing data for file-gen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::SocketAddr;
+use std::time::Instant;
 
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
@@ -11,7 +12,10 @@ use hyper_api_exp::*;
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
 
+    let start = Instant::now();
     fs_tools::generate_file_list().await?;
+    let file_list_dur = start.elapsed();
+    println!("generating file-list.html took {file_list_dur:?}");
 
     let addr: SocketAddr = "127.0.0.1:1336".parse().unwrap();
 
